4d98a83aaafb0063bbfcd3a7b793bf2d9f793049
mateusSR <mateusmsouza04@gmail.com>
2022-10-22 15:49:31 -0300

    backup

--- harry-potter/components/BackToTop/index.js
+++ harry-potter/components/BackToTop/index.js
@@ -1,16 +1,29 @@
+import React, { useEffect, useState } from "react";
 import styles from '../../styles/BackToTop/BackToTop.module.css'
 
 export default function BackToTop () {
 
-    const btn = index.getElementById("btn")
+    const [visible, setVisible] = useState(false)
 
-    btn.addEventListener("click", function (){
-        window.scrollTo(0, 0)
-    })
+    const toggleVisible = () => {
+        const scrolled = window.scrollY;
+        if (scrolled > 300) {
+        setVisible(true)
+        }
+
+        window.addEventListener('scroll', toggleVisible);
+    };
+
+    const scrollToTop = () =>{
+        window.scrollTo({
+            top: 0, 
+            behavior: 'smooth'
+        });
+    };
 
     return (
-        <div className={styles.btn}>
-            <i className={styles.arrow}></i>
+        <div className={styles.btn} onClick={scrollToTop}>
+            {visible ?<i className={styles.arrow} /> :<></>}
         </div>
     )
 }
--- harry-potter/components/Header/index.js
+++ harry-potter/components/Header/index.js
@@ -4,7 +4,7 @@ import styles from '../../styles/Home/Home.module.css'
 
 export default function Header ({logoHeader}) {
     return (
-        <header>
+        <header id="header">
             <div className={styles.divHeader}>
                 {logoHeader && (<div className={styles.divHeader_divImg} onClick={() => Router.push('../')}><img alt="header logo" src='/images/logo-harry-potter.png' className={styles.divHeader_img} /></div>)}
             </div>
